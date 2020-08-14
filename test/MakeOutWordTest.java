baf51a30418cad373de1d2ba8adf5446fdf325cc
Jason Shaner <jshaner@cnm.edu>
2020-08-14 13:59:58 -0600

    Added test cases for problem 2.

--- test/MakeOutWordTest.java
+++ test/MakeOutWordTest.java
@@ -4,7 +4,30 @@ import org.junit.jupiter.api.Test;
 
 class MakeOutWordTest {
 
+  static final String[] makeoutWordParams = {
+
+      //TODO figure out how to arrange the params
+      "<<>>", "Yay",
+      "<<>>", "WooHoo",
+      "[[]]", "word",
+      "HHoo", "Hello",
+      "abyz", "YAY"
+  };
+
+  static final String[] makeoutWordExpected = {
+      "<<Yay>>",
+      "<<WooHoo>>",
+      "[[word]]",
+      "HHHellooo",
+      "abYAYyz"
+  };
+
+
   @Test
   void makeOutWord() {
+    for (int i = 0; i < makeoutWordParams.length; i++) {
+      String actual = HelloName.helloName(makeoutWordParams[i]);
+      assertEquals(makeoutWordExpected[i], actual);
+    }
   }
 }
